44016445349fb6e9855e0439e429586507ebe183
HARINI_VS <harini3508@gmail.com>
2026-07-13 12:21:09 +0530

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] h= new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            h[i]=heights[i];
+        }
+        Arrays.sort(heights);
+        int count=0;
+        for(int i=0;i<h.length;i++){
+            if(h[i]!=heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
